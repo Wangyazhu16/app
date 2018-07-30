baf2789d67959f8dfcce900feb90321aa95f51cf
wangyazhu <wangyazhu@dankegongyu.com>
2018-07-30 15:05:02 +0800

    删除some-mongo

--- some-mongo.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-docker run --rm -it --name some-mongo --net idc -dp 27017:27017 \
-    -e MONGO_INITDB_ROOT_USERNAME=mongo \
-    -e MONGO_INITDB_ROOT_PASSWORD=mongo \
-    -e MONGO_INITDB_DATABASE=admin mongo
